93be97fefa20b57110556841a45be4dd22c107e9
Pierre Jaglin <pjjaglin@gmail.com>
2025-06-17 16:48:11 +0200

    Add article

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.views.defaults import server_error
 
-from .views import index
+from .views import index, article
 
 urlpatterns = [
     path('', index, name="blog-index"),
+    path('article-<str:article_id>/', article, name="blog-article"),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
+
+def article(request, article_id):
+    # In a real application, you would fetch the article from the database
+    # using the article_id. Here we just return a placeholder response.
+    if article_id in ['1', '2', '3']:
+        return render(request, f"blog/article_{article_id}.html")
+    else:
+        return render(request, 'blog/article_not_found.html', status=404)
