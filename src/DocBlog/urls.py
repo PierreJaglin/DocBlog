e98880286bd9f39c752120f31bf34d57aff2e443
Pierre Jaglin <pjjaglin@gmail.com>
2025-06-17 13:55:13 +0200

    Add url for app

--- src/DocBlog/urls.py
+++ src/DocBlog/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.views.defaults import server_error
 
 from DocBlog.views import index
 
 urlpatterns = [
     path('', index, name="index"),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
